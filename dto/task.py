8be65dcd6d61760c0c41cfa5dd0bb86214d71e17
FrankHeels <lock.your.mean@gmail.com>
2026-04-20 20:02:55 +0200

    Добавил статусы для Tasks

--- dto/task.py
+++ dto/task.py
@@ -2,18 +2,20 @@ from pydantic import BaseModel, Field
 from pydantic import ConfigDict
 from typing import Optional
 
+from models.task import TaskStatus
+
 
 class TaskCreate(BaseModel):
     """Валидация входящих данных от клиента"""
     title: str = Field(..., min_length=1, max_length=255)
     description: Optional[str] = Field(default=None, max_length=1024)
-    status: str = Field(default="new")
+    status: TaskStatus = Field(default=TaskStatus.new)
 
 class TaskResponse(BaseModel):
     """Схема для ответа клиенту"""
     id: int
     title: str
     description: Optional[str] = None
-    status: str
+    status: TaskStatus
 
     model_config = ConfigDict(from_attributes=True)
--- models/task.py
+++ models/task.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
 from typing import Optional
+from enum import Enum
 
+class TaskStatus(str, Enum):
+    new = "new"
+    in_progress = "in_progress"
+    completed = "completed"
 
 @dataclass
 class Task:
     id: int
     title: str
-    status: str
+    status: TaskStatus
     description: Optional[str] = None
